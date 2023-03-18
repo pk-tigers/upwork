dd1b3eaebdc18b7d8bf6cef5d4efdf5b125936ac
Rafał Matuszyk <matuszykrafa@gmail.com>
2023-03-18 11:49:19 +0100

    init
    backend init
    frontend gitignore

---
+++ backend/UpWork/UpWork.Api/Controllers/PingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UpWork.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Pong()
+        {
+            return Ok("pong");
+        }
+        [HttpGet("auth"), Authorize]
+        public ActionResult PongAuth()
+        {
+            return Ok(User.Identity?.IsAuthenticated);
+        }
+    }
+}
