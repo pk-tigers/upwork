2031a7b64789fda370f543a4520d55c2d9ea2ba0
Aleksandra <91892932+aleksandragoryczka@users.noreply.github.com>
2023-04-26 23:42:18 +0200

    cleaning

--- frontend/upwork-client/src/app/components/navigation/navigation.component.ts
+++ frontend/upwork-client/src/app/components/navigation/navigation.component.ts
@@ -16,23 +16,4 @@ export class NavigationComponent implements OnInit {
   onItemClick(index: number) {
     this.activeIndex = index;
   }
-
-  /*
-
-  addActiveClassToClickedListItem() {
-    const navItems = document.querySelectorAll('.nav-item');
-    navItems.forEach(item => {
-      item.addEventListener('click', event => {
-        event.preventDefault();
-        navItems.forEach(navItem => navItem.classList.remove('active'));
-        item.classList.add('active');
-        const url = item.querySelector('.nav-link').getAttribute('routerLink');
-        if (url) {
-          this.router.navigateByUrl(url);
-        } else {
-          this.location.back();
-        }
-      });
-    });
-  }*/
 }
