44b9f5836bd8a09b2626ef187ab390f4665764b9
majchpa <126617386+majchpa@users.noreply.github.com>
2023-04-29 12:01:02 +0200

    Add color variables, minor changes

--- frontend/upwork-client/src/app/shared/ui/popup/popup.component.ts
+++ frontend/upwork-client/src/app/shared/ui/popup/popup.component.ts
@@ -1,42 +1,57 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component,Input,OnInit, Inject} from '@angular/core';
 
+import {
+  MatDialog, 
+  MatDialogRef, 
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
-  styleUrls: ['./popup.component.css'],
+  styleUrls: ['./popup.component.scss']
 })
 export class PopupComponent {
-  closePopup() {
-    document.querySelector('.popup-content')?.remove();
+  closePopup(){
+    this.dialogRef.close('primary');
   }
-
-  constructor() {
-    this.width = '500px';
-    this.height = '300px';
-    this.popupTitle = 'Popup';
-    this.popupInfo = 'Confirm choice?';
-    this.btnPrimaryText = 'Accept';
-    this.btnSecondaryText = 'Decline';
-    this.btnSecondaryFunction = this.closePopup;
+  
+  
+  constructor(public dialogRef: MatDialogRef<PopupComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+      this.width="500px";
+      this.height="300px";
+      this.popupTitle="Popup";
+      this.popupInfo="Confirm choice?";
+      this.btnPrimaryText="Accept";
+      this.btnSecondaryText="Decline";
+      this.btnSecondaryFunction=this.closePopup;
   }
 
   ngOnInit(): void {
-    const popup = document.querySelector('.popup-content') as HTMLElement;
+    const popup=document.querySelector(".popup-content") as HTMLElement;
+    if  (this.width)  {
+      popup.style.width=this.data.width;
+    }
+    if  (this.height) {
+      popup.style.height=this.data.height;
+    }
+    
+  
   }
   //Width of the popup with units, default 500px
-  @Input() width: string;
+  @Input("width") width:string;
   //Height of the popup with units, default 300px
-  @Input() height: string;
+  @Input("height") height:string;
   //Title of the popup, default "Popup"
-  @Input() popupTitle: string;
+  @Input("popupTitle") popupTitle:string;
   //Content of the popup, default "Confirm choice?"
-  @Input() popupInfo: string;
+  @Input("popupInfo") popupInfo:string;
   //Text of the primary button, default "Accept"
-  @Input() btnPrimaryText: string;
+  @Input("btnPrimaryText") btnPrimaryText:string;
   //Text of the secondary button, default "Decline"
-  @Input() btnSecondaryText: string;
+  @Input("btnSecondaryText") btnSecondaryText: string;
   //Function that will be executed on clicking primary button, takes no argument and returns none
-  @Input() btnPrimaryFunction: (() => void | undefined) | undefined;
+  @Input("btnPrimaryFunction") btnPrimaryFunction: (() => void | undefined) | undefined;
   //Function that will be executed on clicking secondary button, takes no argument and returns none, default remove popup
-  @Input() btnSecondaryFunction: () => void;
+  @Input("btnSecondaryFunction") btnSecondaryFunction: () => void;
 }
