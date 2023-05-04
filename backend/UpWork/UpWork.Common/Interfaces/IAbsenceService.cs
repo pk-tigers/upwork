b0ccbefce9783946120c69e31d129c048002df72
Rafał Matuszyk <matuszykrafa@gmail.com>
2023-05-04 20:03:06 +0200

    Update IAbsenceService.cs

--- backend/UpWork/UpWork.Common/Interfaces/IAbsenceService.cs
+++ backend/UpWork/UpWork.Common/Interfaces/IAbsenceService.cs
@@ -1,6 +1,10 @@
-﻿namespace UpWork.Common.Interfaces
+﻿using UpWork.Common.Dto;
+using UpWork.Common.Models.DatabaseModels;
+
+namespace UpWork.Common.Interfaces
 {
     public interface IAbsenceService
     {
+        AbsenceModel SetAbsenceApprovalState(AbsenceApprovalStateDto absenceApprovalState);
     }
 }
