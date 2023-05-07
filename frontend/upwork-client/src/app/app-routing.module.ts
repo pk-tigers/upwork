df31221c74c4bb4870280999412fddb76e88239f
Aleksander <91941766+aleksanderkus00@users.noreply.github.com>
2023-05-07 22:48:20 +0200

    UP-50 (#31)
    * Add auth guard
    
    * Fix a bug

--- frontend/upwork-client/src/app/app-routing.module.ts
+++ frontend/upwork-client/src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import { LoginComponent } from './home/feature/login/login.component';
 import { OrganizationControlComponent } from './org/feature/organization-control/organization-control.component';
 import { CalendarComponent } from './home/feature/calendar/calendar.component';
 import { AdminPanelComponent } from './org/feature/admin-panel/admin-panel.component';
+import { authGuard } from './shared/auth/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'admin-panel', component: AdminPanelComponent },
-  { path: 'calendar', component: CalendarComponent },
-  { path: 'organization-control', component: OrganizationControlComponent },
+  {
+    path: '',
+    canActivate: [authGuard],
+    children: [
+      { path: 'admin-panel', component: AdminPanelComponent },
+      { path: 'calendar', component: CalendarComponent },
+      { path: 'organization-control', component: OrganizationControlComponent },
+    ],
+  },
 ];
 
 @NgModule({
