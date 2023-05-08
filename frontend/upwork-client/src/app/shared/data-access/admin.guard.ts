184f790c132d45eb2841c7643824efa3b472a57d
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2023-05-08 19:22:36 +0200

    Fix admin guard bug

--- frontend/upwork-client/src/app/shared/data-access/admin.guard.ts
+++ frontend/upwork-client/src/app/shared/data-access/admin.guard.ts
@@ -1,19 +1,16 @@
 import { inject } from '@angular/core';
 import { UserService } from './user.service';
 import { Router } from '@angular/router';
-import { tap } from 'rxjs';
+import { map } from 'rxjs';
 
 export const adminGuard = () => {
   const userService = inject(UserService);
   const router = inject(Router);
 
   return userService.isAdmin$.pipe(
-    tap(async isAdmin => {
-      if (!isAdmin) {
-        await router.navigateByUrl('/');
-        return false;
-      }
-      return true;
+    map(async isAdmin => {
+      if (!isAdmin) await router.navigateByUrl('/');
+      return isAdmin;
     })
   );
 };
