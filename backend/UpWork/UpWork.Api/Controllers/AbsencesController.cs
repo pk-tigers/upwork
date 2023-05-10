23b83ce7de24adbf1099ed72c9fb96a3757775e3
Aleksandra <91892932+aleksandragoryczka@users.noreply.github.com>
2023-05-10 10:13:53 +0200

    small fixes

--- backend/UpWork/UpWork.Api/Controllers/AbsencesController.cs
+++ backend/UpWork/UpWork.Api/Controllers/AbsencesController.cs
@@ -30,7 +30,6 @@ namespace UpWork.Api.Controllers
         public ActionResult<PaginatedResult<AbsenceModel>> GetAbsencesByUserId(Guid userId, DateTime from, DateTime to, int skip = 0, int take = 10)
         {
             var res = _absencesService.GetAbsencesByUserId(userId, from, to, skip, take);
-            Console.WriteLine("tutaj");
             return Ok(res);
         }
 
