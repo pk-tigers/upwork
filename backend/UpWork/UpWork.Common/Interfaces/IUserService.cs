fb33b5e07ba5cfec24c9b91642733cb0be62bb04
Rafał Matuszyk <matuszykrafa@gmail.com>
2023-05-29 18:59:23 +0200

    code smells

--- backend/UpWork/UpWork.Common/Interfaces/IUserService.cs
+++ backend/UpWork/UpWork.Common/Interfaces/IUserService.cs
@@ -7,7 +7,6 @@ namespace UpWork.Common.Interfaces
     public interface IUserService
     {
         UserModel CreateUser(RegisterDto registerDto, Role role = Role.User);
-        UserModel GetUser(Guid Id, Guid organizationId);
         UserModel GetUser(Guid Id);
         bool DeleteUser(Guid Id);
         UserModel UpdateUser(UserModel existingUser, UpdateUserDto updateUserDto);
